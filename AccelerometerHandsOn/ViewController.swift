5c40ec3a1eb5d9560c0b92724fa7d75760f8b8f9
David <kristoforus_david@outlook.com>
2020-02-07 13:42:32 +0700

    Initial Commit

---
+++ AccelerometerHandsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccelerometerHandsOn
+//
+//  Created by Jasmine Hanifa Mounir on 11/07/19.
+//  Copyright © 2019 Jasmine Hanifa Mounir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
